c7295d80b2e0950eeadae8ac7902b82c5cd6bed4
ProgerDav <davit.gyulnazaryan@tumo.org>
2019-08-27 18:29:06 +0400

    added changes to index

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ItSlide from '../it_slide';
+import Courses from '../Courses/courses';
 
 const Home = props => {
   return (
     <main>
       <ItSlide />
+      <Courses />
     </main>
   );
 }
