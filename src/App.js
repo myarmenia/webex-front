b138a4b92ade8f124137095f1627781925067703
Armine <you@example.com>
2019-08-27 15:04:47 +0400

    starting project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+
 import React from 'react';
-import logo from './logo.svg';
+import './styles/bootstrap.min.css';
+import './styles/animations.css';
+import './styles/font-awesome.css';
+import './styles/main.css';
+import ItSlide from './Components/it_slide'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItSlide/>
     </div>
   );
 }
