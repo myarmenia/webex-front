2c9dff3e425825e56c37cbd83bf87d5795e4135c
TigranBatoyan <51286422+TigranBatoyan@users.noreply.github.com>
2019-08-30 17:53:04 +0400

    added material-ui

---
+++ src/Components/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ItSlide from '../it_slide';
+import Courses from '../Courses/courses';
+import SignIn from '../Header/SignIn/index.js'
+const Home = props => {
+  return (
+    <main>
+      <ItSlide />
+      <Courses />
+      
+    </main>
+  );
+}
+
+export default Home;
