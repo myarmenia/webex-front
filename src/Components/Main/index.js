8fcc5f8a37bba7075729178b223ca57d7dff97b4
Narek Sargsyan <sargsyansargsyan310@gmail.com>
2019-08-30 18:01:09 +0400

    narek_input_styles

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ItSlide from '../it_slide';
 import Courses from '../Courses/courses';
+import SignUp from '../Header/SignUp'
 
 const Home = props => {
   return (
     <main>
       <ItSlide />
+      <SignUp />
       <Courses />
     </main>
   );
