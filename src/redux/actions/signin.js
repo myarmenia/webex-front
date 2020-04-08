aee4c27ae99e779a63bf07afd14ce4a86bf8dde6
ProgerDav <davit.gyulnazaryan@tumo.org>
2020-04-08 15:52:47 +0400

    Added activations in currentUser reducer, also created types for currentUser

--- src/redux/actions/signin.js
+++ src/redux/actions/signin.js
@@ -1,14 +1,22 @@
+import {
+  USERDATA_SUCCESS,
+  SIGNIN_SUCCESS,
+  SIGNIN_REQUEST,
+  SIGNIN_ERROR,
+} from "../types/currentUser";
 
-export const SignInSuccess = user => {
-  return { type: 'SIGNIN_SUCCESS', payload: user };
-}
+export const UserDataSuccess = (data) => {
+  return { type: USERDATA_SUCCESS, payload: data };
+};
 
-export const SignInError = errorMessage => {
-  return { type: 'SIGNIN_ERROR', errorMessage };
-}
+export const SignInSuccess = (user) => {
+  return { type: SIGNIN_SUCCESS, payload: user };
+};
+
+export const SignInError = (errorMessage) => {
+  return { type: SIGNIN_ERROR, errorMessage };
+};
 
 export const SignInRequest = () => {
-  return { type: 'SIGNIN_REQUEST' }
-}
-
-
+  return { type: SIGNIN_REQUEST };
+};
--- src/redux/reducers/currentUser.js
+++ src/redux/reducers/currentUser.js
@@ -1,19 +1,40 @@
+import {
+  USERDATA_SUCCESS,
+  SIGNIN_SUCCESS,
+  SIGNIN_REQUEST,
+  SIGNIN_ERROR,
+} from "../types/currentUser";
+
 const initialState = {
   authenticated: false,
   loading: false,
-  error: '',
-  user: {}
+  error: "",
+  user: {},
 };
 
 export default function currentUser(state = initialState, action) {
   switch (action.type) {
-    case "SIGNIN_REQUEST":
+    case SIGNIN_REQUEST:
       return { ...state, loading: true };
 
-    case "SIGNIN_SUCCESS":
-      return { ...state, authenticated: true, loading: false, user: action.payload };
+    case USERDATA_SUCCESS:
+      return {
+        ...state,
+        authenticated: true,
+        loading: false,
+        user: action.payload.user,
+        activations: action.payload.activations,
+      };
 
-    case "SIGNIN_ERROR":
+    case SIGNIN_SUCCESS:
+      return {
+        ...state,
+        authenticated: true,
+        loading: false,
+        user: action.payload,
+      };
+
+    case SIGNIN_ERROR:
       return { ...state, loading: false, error: action.errorMessage };
 
     default:
