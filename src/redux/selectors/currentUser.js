4b25fb5c929975f725ee728b8b2fa88dec2aeb76
ProgerDav <davit.gyulnazaryan@tumo.org>
2020-04-28 17:01:39 +0400

    Checking if the activation are loaded, based on new attribute in reducer initial state

--- src/redux/selectors/currentUser.js
+++ src/redux/selectors/currentUser.js
@@ -1,15 +1,18 @@
 import { createSelector } from "reselect";
 
 export const activationsSelector = (state) => state.currentUser.activations;
+export const activationsLoadedSelector = (state) =>
+  state.currentUser.activations_loaded;
 
 export const currentActivationSelector = createSelector(
   activationsSelector,
-  (activations) => {
+  activationsLoadedSelector,
+  (activations, loaded) => {
     const currentActivation =
       activations.filter((activation) => !activation.is_expired).reverse()[0] ||
       null;
 
-    if (!currentActivation && activations.length) return false;
+    if (loaded && !currentActivation) return false;
 
     return currentActivation;
   }
