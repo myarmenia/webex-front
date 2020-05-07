0462105deb6ed736608f834d659feace0ff10f0e
adved85 <44031303+adved85@users.noreply.github.com>
2020-05-07 15:11:09 +0400

    set-profile-default-image-from-local-src (#94)

--- src/pages/Profile/Pages/UserProfile/Lg4/Block1/index.js
+++ src/pages/Profile/Pages/UserProfile/Lg4/Block1/index.js
@@ -1,26 +1,27 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { useSelector } from "react-redux";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 /** name, lastName, image */
+
+import DefaultImage from "../../../../../../img/news/avatar.png";
 
 function Block1({ name, lastName, online }) {
   let data = useSelector((state) => state.switchingState);
-  let educationType = online ? 'online' : 'stationary';
+  let educationType = online ? "online" : "stationary";
 
   return (
     <Grid className="pt-38px mb-20" container>
       <Grid item lg={4}>
-        <img src={data.userImageSrc} alt={data.userImageAlt} />
+        <img src={DefaultImage} alt={data.userImageAlt} />
       </Grid>
       <Grid className="pl-20px" item lg={8}>
-        <Link className="colorProfilePage" to="/profile" >
-        <strong>{name + " " + lastName}</strong>
+        <Link className="colorProfilePage" to="/profile">
+          <strong>{name + " " + lastName}</strong>
         </Link>
-        <Grid >
-        <code >{educationType}</code>
+        <Grid>
+          <code>{educationType}</code>
         </Grid>
-  
       </Grid>
     </Grid>
   );
