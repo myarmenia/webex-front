80ec0bf0410e660250f2c68fbfc061268f4175fd
adved85 <adved85@gmail.com>
2020-05-23 15:49:17 +0400

    payment-online-done

---
+++ src/pages/Profile/Pages/UserProfile/Lg8/TabPanelPaymentOnline/currencies.js
@@ -0,0 +1,7 @@
+export default [
+  { nbr: "051", code: "AMD", multiplier: 1 },
+  { nbr: "978", code: "EUR", multiplier: 520 },
+  { nbr: "840", code: "USD", multiplier: 480 },
+  { nbr: "643", code: "RUB", multiplier: 6.6 },
+];
+
